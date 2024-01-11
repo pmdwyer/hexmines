60334d86fde4f4717a27d762d9a5f7520bef6939
Patrick Dwyer <dwyer.m.patrick@gmail.com>
2024-01-10 21:53:02 -0500

    Add flood clear on click for grid

--- grid/grid.gd
+++ grid/grid.gd
@@ -14,11 +14,10 @@ func _ready():
 	var random = RandomNumberGenerator.new()
 	random.randomize()
 	for i in range(width):
-		_tiles.append([])
 		for j in range(height):
 			var t = _tile.instantiate()
 			t.set_coords(i, j)
-			_tiles[i].append(t)
+			_tiles.append(t)
 			add_child(t)
 	_setup_mines()
 
@@ -26,23 +25,56 @@ func _ready():
 func _setup_mines():
 	_num_mines = (width * height) / 10
 	for i in range(_num_mines):
-		var x = randi() % width
-		var y = randi() % height
-		_create_mine(x, y)
+		var t = randi() % (width * height)
+		_create_mine(t)
 
 
-func _create_mine(x: int, y: int):
-	_tiles[x][y].is_mine = true
-	var neighbors = _tiles[x][y].get_neighbors()
+func _create_mine(i: int):
+	_tiles[i].is_mine = true
+	var neighbors = _get_neighbors(_tiles[i])
 	for n in neighbors:
-		if n[0] >= 0 && n[0] < width && n[1] >= 0 && n[1] < height:
-			_tiles[n[0]][n[1]].num_neighbor_mines += 1
+		n.num_neighbor_mines += 1
 
 
 func _process(delta):
 	var mouse_pos = get_viewport().get_mouse_position()
-	for row in _tiles:
-		for t in row:
-			if t.mouse_hit(mouse_pos):
-				%is_mine_value.text = str(t.is_mine)
-				%num_mines_value.text = str(t.num_neighbor_mines)
+	var tile = _get_tile_at(mouse_pos)
+	if tile:
+		%is_mine_value.text = str(tile.is_mine)
+		%num_mines_value.text = str(tile.num_neighbor_mines)
+
+
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		var mouse_pos = get_viewport().get_mouse_position()
+		var tile = _get_tile_at(mouse_pos)
+		_flood_clear(tile)
+
+
+func _flood_clear(start):
+	var queue = [start]
+	var seen = {}
+	while len(queue) > 0:
+		var tile = queue.pop_front()
+		for n in _get_neighbors(tile):
+			if n not in seen and not n.cleared and n.num_neighbor_mines == 0:
+				queue.push_back(n)
+		seen[tile] = true
+		tile.clear()
+
+
+func _get_tile_at(mouse_pos):
+	for t in _tiles:
+		if t.mouse_hit(mouse_pos):
+			return t
+	return null
+
+
+func _get_neighbors(tile):
+	var neighbors = []
+	var neighbor_coords = tile.get_neighbor_coords()
+	for nc in neighbor_coords:
+		for t in _tiles:
+			if t.coords == nc:
+				neighbors.append(t)
+	return neighbors
--- tile/tile.gd
+++ tile/tile.gd
@@ -3,39 +3,52 @@ extends Node2D
 
 @export var is_mine: bool = false
 @export var num_neighbor_mines: int = 0
-@export var is_flagged: bool = false;
+@export var is_flagged: bool = false
+@export var coords: Vector2i
+@export var cleared: bool = false
 
 
 var _global_x: int = 0
 var _global_y: int = 0
-var _i: int = 0
-var _j: int = 0
 var _size
 
 
 func set_coords(i: int, j: int):
-	_i = i
-	_j = j
+	coords = Vector2i(i, j)
 	# TODO clean up magic numbers
-	_global_x = (_i * 32) + 64
-	_global_y = (_j * 96) + 64
-	if (_i % 2) == 1:
+	_global_x = (coords.x * 32) + 64
+	_global_y = (coords.y * 96) + 64
+	if (coords.x % 2) == 1:
 		_global_y += 48
 	position = Vector2i(_global_x, _global_y)
 
 
 func mouse_hit(mouse_pos: Vector2) -> bool:
-	return $Sprite2D.get_rect().has_point(to_local(mouse_pos))
+	if $Sprite2D:
+		return $Sprite2D.get_rect().has_point(to_local(mouse_pos))
+	return false
 
 
-func get_neighbors():
-	var neighbors = [[_i - 2, _j], [_i + 2, _j], [_i - 1, _j], [_i + 1, _j]]
-	if _i % 2 == 0:
-		neighbors.append([_i - 1, _j - 1])
-		neighbors.append([_i + 1, _j - 1])
+func clear():
+	if not cleared:
+		cleared = true
+		$Sprite2D.queue_free()
+		remove_child($Sprite2D)
+
+
+func get_neighbor_coords():
+	var neighbors = [
+		Vector2i(coords.x - 2, coords.y),
+		Vector2i(coords.x + 2, coords.y),
+		Vector2i(coords.x - 1, coords.y),
+		Vector2i(coords.x + 1, coords.y)
+	]
+	if coords.x % 2 == 0:
+		neighbors.append(Vector2i(coords.x - 1, coords.y - 1))
+		neighbors.append(Vector2i(coords.x + 1, coords.y - 1))
 	else:
-		neighbors.append([_i - 1, _j + 1])
-		neighbors.append([_i + 1, _j + 1])
+		neighbors.append(Vector2i(coords.x - 1, coords.y + 1))
+		neighbors.append(Vector2i(coords.x + 1, coords.y + 1))
 	return neighbors
 
 
