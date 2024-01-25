4d946f73ea57d99b94cfd56a348aec03758aa4c3
Patrick Dwyer <dwyer.m.patrick@gmail.com>
2024-01-24 21:19:47 -0500

    Add basic time tracking

--- main_game.gd
+++ main_game.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 var grid_scene: PackedScene = preload("res://grid/grid.tscn")
 var _grid
+var _total_time: float = 0.0
 
 
 func _ready():
@@ -11,13 +12,22 @@ func _ready():
 
 
 func _process(delta):
+	_total_time += delta
+	%TimeLabel.text = "%.2f" % _total_time
 	if _grid.game_over():
+		var completed_time = ""
+		if _grid.hit_mine:
+			completed_time = "Lost game in:\t%.2f\n" % _total_time
+		else:
+			completed_time = "Won game in:\t%.2f\n" % _total_time
+		%CompletedTimesList.append_text(completed_time)
 		remove_child(_grid)
 		_create_grid()
 		add_child(_grid)
 
 
 func _create_grid():
+	_total_time = 0.0
 	_grid = grid_scene.instantiate()
 	_grid.width = 20
 	_grid.height = 5
